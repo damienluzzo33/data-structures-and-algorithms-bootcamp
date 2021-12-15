73340ec03a4664a94b4928ef767caadbb9acac2c
Ethan C <ethan.cho1826@gmail.com>
2021-12-15 14:14:50 -0600

    isSubsequence multiple pointer algorithm

---
+++ Day-2/isSubsequence.js
@@ -0,0 +1,24 @@
+/* Write a function called isSubsequence which takes in two strings and checks whether the characters in the first string form a subsequence of the characters in the second string. 
+In other words, the function should check whether the characters in the first string appear somewhere in the second string, without their order changing.
+function will take in 2 strings
+it will do a comparison to make sure one string is a substring of the other
+*/
+
+function isSubsequence(str1, str2) {
+    // TODO: Create pointers that start at the beginning of both strings
+    
+    // TODO: Catch the edge case of if the substring string inputted was empty
+    
+
+    // TODO: Continue looping as long as the main string pointer doesn't reach the end of the main string
+    while () {
+        // TODO: If the characters are the same at the current pointers of each string, move the pointer on the substring
+        
+        // TODO: If the pointer on the substring reaches the end of the substring return true
+        
+        // TODO: increment the pointer on the main string
+        
+    }
+    // TODO: if while loop was finished what is returned here?
+    
+}
