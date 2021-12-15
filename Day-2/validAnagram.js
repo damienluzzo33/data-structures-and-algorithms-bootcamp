71a390f565a84a800e37a77c66cbb30c4f97c171
Ethan C <ethan.cho1826@gmail.com>
2021-12-15 13:28:34 -0600

    gave more todos

--- Day-2/validAnagram.js
+++ Day-2/validAnagram.js
@@ -6,8 +6,9 @@ An anagram is a word, phrase, or name formed by rearranging the letters of anoth
 */
 
 function validAnagram(str1, str2){
-    let frequencyCounter1 = {};
-    let frequencyCounter2 = {};
+    // TODO: Create frequency counters to keep track of each characters in both strings
+    
+
     for(let char of str1) {
         // TODO: Increase the counter of the current char in str1 in the loop
         
@@ -15,7 +16,8 @@ function validAnagram(str1, str2){
     for(let char of str2) {
         // TODO: Increase the counter of the current char in str2 in the loop
     }
-    for (let char in frequencyCounter1) {
+    // TODO: Loop over the frequency counter keys to see each key value
+    for (let char in ) {
         // TODO: check to see if there are any missing characters in either frequency counters, if so return false
         
         // TODO: check to see if there are the same amount of characters in both strings, if not return false
