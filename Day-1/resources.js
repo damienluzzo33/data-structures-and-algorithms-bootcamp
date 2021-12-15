4eb7a27159bf8708b7a79d04c948b7bd7cefd86d
Ethan C <ethan.cho1826@gmail.com>
2021-12-15 03:26:54 -0600

    added more resources on big O

--- Day-1/resources.js
+++ Day-1/resources.js
@@ -8,3 +8,9 @@
 //@  Time Complexity Performance Tracker: 
 //@  https://rithmschool.github.io/function-timer-demo/
 
+//@ For those wanting a more "CS"-y way of explaining big O:
+//@ timestamped @ 52:47 but you're more than welcome to watch their sections on the different algorithms
+//@ https://www.youtube.com/watch?v=U9o49qwa6hk&t=3167s
+
+//@ Colt Steele's way of teaching big O:
+//@ https://www.youtube.com/watch?v=kS_gr2_-ws8
