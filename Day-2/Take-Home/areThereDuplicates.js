c2d6324fc57bc695915f68728c8bc80e56d1d80b
Ethan C <ethan.cho1826@gmail.com>
2021-12-15 15:50:49 -0600

    take home algorithms

---
+++ Day-2/Take-Home/areThereDuplicates.js
@@ -0,0 +1,19 @@
+// Implement a function called, areThereDuplicates which accepts a variable number of arguments, and checks whether there are any duplicates among the arguments passed in
+// Can solve this using either Frequency Counter or Multiple Pointers
+// variable number of arguments means to use the built-in argument object in javascript
+// if using multiple pointers the array must be sorted first!
+
+// By variable number of arguments, search up the arguments object on MDN! https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Functions/arguments
+
+// Frequency Counter Approach
+// HINT: Do we need to declare parameters?
+function areThereDuplicates() {
+
+}
+
+// Multiple Pointers Approach
+// HINT: What must the array be before you can use multiple pointers?
+// HINT: Where should you start your multiple pointers?
+function areThereDuplicates() {
+    
+}
