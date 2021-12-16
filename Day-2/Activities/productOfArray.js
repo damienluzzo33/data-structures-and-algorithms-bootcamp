dfff04349935ad3878bc3d1598ac3805e3c6e7c6
Ethan C <ethan.cho1826@gmail.com>
2021-12-16 01:57:36 -0600

    product of arrays recursion

---
+++ Day-2/Activities/productOfArray.js
@@ -0,0 +1,21 @@
+//* write a function that will give you the product of all items in the array recursively
+//* (Hint) use a helper function to affect the running total
+
+function productOfArray(arr) {
+    // TODO: Declare your running total
+
+    // TODO: Define your helper function that will take in an input, and affect the running total variable
+    function helper() {
+        // TODO: Declare your base case here to stop an endless recursion
+
+        // TODO: Write the product of result with the current array item
+
+        // TODO: Write the recursion here
+
+    }
+    // TODO: Invoke the helper function passing in the array
+
+    // TODO: Return the running total
+}
+
+// TODO: Invoke productOfArray with a correct input
