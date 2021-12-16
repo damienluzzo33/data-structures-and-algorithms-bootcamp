365a02d858011ed6bd3ddff0f3a8df0546834938
Ethan C <ethan.cho1826@gmail.com>
2021-12-16 14:32:00 -0600

    more solutions

---
+++ Day-2/Walkthrough-Solved/zeroOutSolution.js
@@ -0,0 +1,14 @@
+function sumZero(arr) {
+    let left = 0;
+    let right = arr.length - 1;
+    while(left < right) {
+        let sum = arr[left] + arr[right];
+        if(sum === 0 ) {
+            return [arr[left], arr[right]];
+        }else if(sum < 0) {
+            left++;
+        }else {
+            right--;
+        }
+    }
+}
