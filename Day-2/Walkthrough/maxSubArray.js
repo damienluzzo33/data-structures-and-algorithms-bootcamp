30e47903ad4a90622f107e3463f8fdc57d41a4a0
Damien Luzzo <damienluzzo33@gmail.com>
2021-12-16 03:08:51 -0600

    create sliding window activity and walkthrough

---
+++ Day-2/Walkthrough/maxSubArray.js
@@ -0,0 +1,14 @@
+//*  Create a function called "maxSum" that accepts an two inputs: one that is an array of positive integers and a second one that is just an integer. The function should return the maximum sum of consecutive integers based on the second input that is passed in.
+
+//*  Test Cases:
+//*  maxSum( [50, 100, 150, 200, 250], 3 ) --> 600
+//*  maxSum( [51, 12, 10, 16, 92], 4 ) --> 130
+//*  maxSum( [1, 6, 10, 3, 5], 3 ) --> 19
+
+//*  What is being asked here?
+//*  What types of inputs are we using?
+//*  Are there any edge cases we have to look out for?
+//*  What should the output be?
+//*  What are we keeping track of?
+//*  Break it down, pseudo code it, and solve it
+
