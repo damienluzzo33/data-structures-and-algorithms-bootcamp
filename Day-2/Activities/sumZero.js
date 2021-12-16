847d57620aa51869dc6188c3a33b55c0e8f6c18e
Ethan C <ethan.cho1826@gmail.com>
2021-12-16 02:00:25 -0600

    commiting to unstage unwanted changes

---
+++ Day-2/Activities/sumZero.js
@@ -0,0 +1,33 @@
+
+//*  Write a function called sumZero which accepts a sorted array of integers. 
+//*  The function should find the first pair where the sum is 0. 
+//*  Return an array that includes both values that sum to zero or undefined if a pair does not exist.
+
+//*  Test Cases: 
+//*  sumZero([-3, -2, -1, 0, 1, 2, 3]) returns [-3, 3]
+//*  sumZero([-2, -1, 0, 1, 2, 3]) returns undefined
+//*  sumZero([1, 2, 3]) returns undefined
+
+function sumZero(arr) {
+    // TODO: Create two pointers starting on opposite ends of the array
+    
+    //TODO: Continue to loop as long as the pointers never cross each other in the array
+    while() {
+        // TODO: Add the elements that correspond to the two pointers and store it in a variable
+        
+        // TODO: Check to see if the sum of the two elements equals 0, if so, return the pair
+        if(sum === 0 ) {
+            
+            // TODO: if the sum of the two elements is less than 0, move the beginning pointer to the right
+        }else if() {
+            
+            // TODO: if the sum of the two elements is greater than 0, move the end pointer to the left
+        }else if() {
+            
+        }
+    }
+}
+
+
+// TODO: call the "sumZero" function using a valid input
+sumZero();
