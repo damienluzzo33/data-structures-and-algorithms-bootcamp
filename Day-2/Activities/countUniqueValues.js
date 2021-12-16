31697e031e2083d31a8296daa7f1cb39ae43263c
Damien Luzzo <damienluzzo33@gmail.com>
2021-12-16 17:13:00 -0600

    style final walkthroughs and activities

--- Day-2/Activities/countUniqueValues.js
+++ Day-2/Activities/countUniqueValues.js
@@ -5,32 +5,13 @@
 //*  There can be negative numbers in the array, but it will always be sorted.
 
 //*  Test Cases:
+
 //*  [1, 1, 1, 1, 1, 2] returns 2
-//*  [1, 2, 3, 4, 4, 4, 7, 7, 12, 12, 13] returns 7
+
+
+//*  [1, 2, 3, 4, 7, 12, 13, 4, 4, 7, 12, 12] returns 7
+
 //*  [] returns 0
-//*  [-2, -1, -1, 0, 1] returns 4
 
+//*  [-2, -1, 0, 1, -1] returns 4
 
-function countUniqueValues(arr) {
-    // TODO: Account for the edge case of an empty array
-    // TODO: Declare your two pointers, but instead of making them start at the beginning and end, make them start next to each other at the beginning
-    
-    // TODO: Continue to loop as long as your furthest pointer doesn't go past the array
-    while() {
-        // TODO: If the elements at the two pointers are not equal (they're unique!), 
-        
-        // TODO Tips - increment the beginning pointer, 
-        // TODO Tips - swap the elements at the current positions of the two pointers, 
-        // TODO Tips- increment the second pointer 
-        if () {
-            
-            // TODO: If they aren't unique, move the furthest pointer
-        } else {
-            
-        }
-    }
-    // TODO: Return the length of the stored unique values at the beginning of the array (in other words, the amount of unqiue values in the array)
-}
-
-// TODO: call the "countUniqueValues" function with a valid input
-countUniqueValues();
--- Day-2/Activities/validAnagram.js
+++ Day-2/Activities/validAnagram.js
@@ -9,7 +9,6 @@ function validAnagram(string, anagram){
     // TODO: Edge case of string and anagram not being the same length
 
     // TODO: Create frequency counters to keep track of each characters in both strings
-    
 
     for(let char of string) {
         // TODO: Increase the counter of the current char in str1 in the loop
@@ -17,7 +16,9 @@ function validAnagram(string, anagram){
     }
     for(let char of anagram) {
         // TODO: Increase the counter of the current char in str2 in the loop
+
     }
+
     // TODO: Loop over the frequency counter keys to see each key value
     for (let char in ) {
         // TODO: check to see if there are any missing characters in either frequency counters, if so return false
