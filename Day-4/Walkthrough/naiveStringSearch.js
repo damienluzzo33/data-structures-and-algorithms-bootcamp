70ae68e0893df7bbae5fb1ddfd4e00d4feb618c8
Damien Luzzo <damienluzzo33@gmail.com>
2021-12-19 16:48:22 -0600

    rename day 2 folder to Day-2-and-3, and add day 4 walkthrough activities

---
+++ Day-4/Walkthrough/naiveStringSearch.js
@@ -0,0 +1,12 @@
+//? NAIVE STRING SEARCH
+
+//* Create a function called naiveSearch that accepts a long string called "text" and a shorter string called "pattern" that should return the total number of times that the pattern is found in the text. 
+
+//* What are the steps involved with solving this problem? 
+
+//* How do we test if the values match?
+
+//* What do we do once our loops identify a match?
+
+//* What is it that we are looking to return in the end? How to we keep track of that?
+
