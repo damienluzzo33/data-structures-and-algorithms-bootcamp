3a5e04dde932fc849bc7a91fa22eb0480b179765
Damien Luzzo <damienluzzo33@gmail.com>
2021-12-21 01:34:53 -0600

    add TODOs to the in class activities

--- Day-5/Activities/insertionSort.js
+++ Day-5/Activities/insertionSort.js
@@ -6,10 +6,28 @@
 
 //@ Hint #2: this can be done using a while loop OR a nested for loop. 
 
-//@ Hint #3: If you are using a nested for loop, there may be scoping issues that develop depending how you plan to swap the values after the second for-loop has completed. Because of this, we recommend using the "var" keyword instead of "let" to define your inner looping variable (i.e. --> for (var j = ...; j < ...; j--))
+//! CAUTION: If you are using a nested for loop, there may be scoping issues that develop depending how you plan to swap the values after the second for-loop has completed. Because of this, we recommend using the "var" keyword instead of "let" to define your inner loop variable (i.e. --> for (var j = ...; ...; ...))
 
 function insertionSort(arr) {
-    
+    for (var i = 1; i < arr.length; i++) {
+        // TODO: Create a variable called currentValue and assign it the value of the array at i
+
+        // TODO: Create a nest for loop (easier) or a while loop (with an additional variable to create an variable "index tracker" called j)
+
+        //! This part can be tricky! You want to decrement your looping from the value to the left of i until (and including) the 0 index 
+        // var j = ... 
+        // while () {}
+        for () {
+            // TODO: Check to see if the value at j is greater than the currentValue
+            if () {
+                // TODO: If it is, then we want to move that value to the right
+            } else {
+                // TODO: Otherwise break out of the loop
+            }
+        }
+        // TODO: Now, j is either pointing to a negative index, or at a value that is less than the currentValue, so we want to put the current value back in the array FOLLOWING the index currently being pointed at
+    }
+    // TODO: Return the array
 }
 
 console.log(insertionSort([15,4,13,2,11]));
--- Day-5/Activities/selectionSort.js
+++ Day-5/Activities/selectionSort.js
@@ -11,7 +11,22 @@
 //*  [arr[index1], arr[index2]] = [arr[index2], arr[index1]];
 
 function selectionSort(arr) {
+    for (let i = 0; i < arr.length; i++) {
+        // TODO: Create a variable called lowestIndex that starts off equal to the value of i
 
+        // TODO: Create a nested for loop that iterates from the index after the current value of i to the end of the array
+        for () {
+            // TODO: Check to see if the value at j is less than the value of the current lowest index
+            if () {
+                // TODO: if so, we need to update the lowest index
+            }
+        }
+        // TODO: Check to see if the value of currentIndex has changed
+        if () {
+            // TODO: If it has changed, then we need to swap the values
+        }
+    }
+    // TODO: return the sorted array
 }
 
 console.log(selectionSort([15,4,13,2,11]));
