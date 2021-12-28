1f6d9b81de47d1613e50df978a7cbaaddd974210
Damien Luzzo <damienluzzo33@gmail.com>
2021-12-27 21:57:43 -0600

    add final sorting algorithm to the walkthrough folder for Day 8 and 9

---
+++ Day-8-and-9/Walkthrough/countingSort.js
@@ -0,0 +1,35 @@
+//?  COUNTING SORT
+
+//* Create a function called countingSort that takes an array, a minimum value, and a maximum vue, and returns a sorted array
+
+//* Main Function: Use a frequency counter for this one!
+
+function countingSort (arr, min, max) {
+
+    // TODO: define a variable that starts at 0 and another one to use as your frequency counter (the frequency counter can be an object or an array but might be easier if it's an array)
+
+    // TODO: loop through the array
+    for () {
+        
+        // TODO: for every element in the array, find it in the frequency counter and increment it
+
+    }
+
+    // TODO: loop from the min to the max value
+    for () {
+
+        // TODO: while that index corresponds to a non-zero frequency
+        while () {
+            // TODO: override the input array with that value at the index that corresponds to the variable we created at the beginning
+            
+            // TODO: increment that variable and decrement the frequency value
+            
+        }
+    }
+    // TODO: return the sorted array
+    
+};
+
+
+console.log(countingSort([2,10,4,6,13,15,6,8,4,6], 1, 16))
+//* Expected Output: [ 2, 4, 4, 6, 6, 6, 8, 10, 13, 15 ]
